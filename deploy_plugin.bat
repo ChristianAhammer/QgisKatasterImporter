072c15488b602b896dfd3d3385011ac5d94e8dd4
ChristianAhammer <125205166+ChristianAhammer@users.noreply.github.com>
2025-07-30 18:33:45 +0200

    Create deploy_plugin.bat

---
+++ deploy_plugin.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+echo Kopiere Plugin...
+
+REM Quelle: Repo-Pfad
+set SOURCE=C:\QgisData\GitRepos\kataster_converter
+
+REM Ziel: QGIS Plugin-Verzeichnis
+set TARGET=%APPDATA%\QGIS\QGIS3\profiles\default\python\plugins\kataster_converter
+
+REM Vorherige Version löschen
+rmdir /S /Q "%TARGET%"
+
+REM Kopieren (inkl. Unterordner, still)
+xcopy "%SOURCE%" "%TARGET%" /E /I /Y >nul
+
+echo Fertig. Plugin wurde aktualisiert.
+pause
